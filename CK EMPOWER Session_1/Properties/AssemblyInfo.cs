5385525a5361a89b4b82f715e041d23d63d1b334
craigking2012 <craigking2012@gmail.com>
2023-05-16 14:33:27 -0400

    Add project files.

---
+++ CK EMPOWER Session_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CK EMPOWER Session_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("CK EMPOWER Session_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("12CAD49A-BD80-4154-8102-7A2806C3B166")]
+[assembly: AssemblyVersion("1.0.0.0")]
